9975bd4db31c188bdbccba35536043967adf60c1
Jung TaeSeung <mike415415@naver.com>
2026-06-06 23:54:02 +0900

    hotfix : DenchAuthType 일반 export로 변경

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import { dench } from './denchfetch/dench';
+import { DenchAuthType } from './types/denchEnum';
+import { HTTPCache } from './types/denchHTTPEnum';
+
 export { dench } from './denchfetch/dench';
 export { default as denchfetcher } from './denchfetch/denchfetcher';
 export { HTTPCredentials, HTTPMode, HTTPReferrerPolicy, HTTPCache, HTTPRedirect } from './types/denchHTTPEnum';
+export { DenchAuthType } from './types/denchEnum';
 export type {  DenchRunner }  from './types/denchRunner';
 export type { DenchBuilder, DenchCreateBuilder, DenchGetBuilder } from './types/denchBuilder';
 export type { DenchConfig } from './types/denchConfig';
-export type { DenchAuthType} from './types/denchEnum';
 export type { DenchInterface, DenchHTTPURL } from './types/dench';
